48a7e16c7bf8d8648c687dc43c30296b1c207c8e
Tamilselvan K <tamizh951437@gmail.com>
2023-11-07 17:31:58 +0530

    initial commit

---
+++ predict.py
@@ -0,0 +1,23 @@
+from cog import BasePredictor, Input, Path
+from PIL import Image
+from rembg import remove
+import tempfile
+
+
+class Predictor(BasePredictor):
+    def predict(self) -> Path:
+        input_image_path: Path = Input(description="Input file", default=None)
+        if not input_image_path:
+            raise ValueError("No input image selected")
+
+        input_image = Image.open(input_image_path)
+
+        output_image = remove(input_image)
+
+        output_path = Path(tempfile.mkdtemp()) / "output.png"
+        output_image.save(output_path)
+
+        return Path(output_path)
+
+
+
