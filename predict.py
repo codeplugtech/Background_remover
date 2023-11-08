5ee5072037259fb631e3e8fb485c903627d65739
tzktz <tamizh951437@gmail.com>
2023-11-08 16:47:34 +0530

    initial pip

--- predict.py
+++ predict.py
@@ -10,14 +10,19 @@ class Predictor(BasePredictor):
         if not input_image_path:
             raise ValueError("No input image selected")
 
+        print("Input image path:", input_image_path)
+
         input_image = Image.open(input_image_path)
 
+        print("Processing input image...")
+
         output_image = remove(input_image)
+
+        print("Image processing complete.")
 
         output_path = Path(tempfile.mkdtemp()) / "output.png"
         output_image.save(output_path)
 
+        print("Output image saved at:", output_path)
+
         return Path(output_path)
-
-
-
